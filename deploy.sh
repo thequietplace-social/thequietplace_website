14f6f418c2bee126ff015d3154a8c7bcb2735d03
KemoNine <mcrosson@kemonine.info>
2017-11-15 20:32:25 -0500

    Update dev details

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 bundle exec jekyll clean
 bundle exec jekyll build
 cd _site
-ln -s /srv/tracker-proxy/thequietplace.social/config.php config.php
+ln -s /srv/tracker-proxy/kemonine.info/config.php config.php
--- jekyll.sh
+++ jekyll.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 bundle exec jekyll clean
-bundle exec jekyll serve --config _config.yml,_config_dev.yml --watch --drafts --incremental --force_polling -H 192.168.159.149
+bundle exec jekyll serve --config _config.yml,_config_dev.yml --watch --drafts --incremental --force_polling -H 192.168.159.151
